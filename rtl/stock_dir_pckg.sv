0957d93f2b09f41d7f4e9dfb4170f328704dea94
LucaRoescheisen <luca.roescheisen@gmail.com>
2026-02-21 16:19:02 +1100

    added stock directory

---
+++ rtl/stock_dir_pckg.sv
@@ -0,0 +1,147 @@
+package stock_dir_pckg
+
+typedef enum logic [7:0] {
+    //NASDAQ Market
+    NASDAQ_GLOBAL_SELECT_MARKET = "Q" ,
+    NASDAQ_GLOBAL_MARKET        = "G" ,
+    NASDAQ_CAPTIAL_MARKET       = "S" ,
+    //Non-NASDAQ instruments
+    NYSE                   = "N" ,
+    NYSE_AMERICAN          = "A" ,
+    NYSE_ARCA              = "P" ,
+    BATS_Z_EXCHANGE        = "Z" ,
+    INVERSTORS_EXCHANGE    = "V",
+    UNKNOWN                = "?"
+
+  } dir_market_category_e;
+
+  typedef enum logic [7:0] {
+    DEFICIENT                     = "D",
+    DELINQUENT                    = "E",
+    BANKRUPT                      = "Q",
+    SUSPENDED                     = "S",
+    DEFICIENT_BANKRUPT            = "G",
+    DEFICIENT_DELINQUENT          = "H",
+    DELINQUENT_BANKRUPT           = "J",
+    DEFICIENT_DELINQUENT_BANKRUPT = "K",
+    SUSPENDED_FOR_ETP             = "C",
+    NORMAL                        = "N"
+  } dir_financial_status_indicator_e;
+
+  typedef enum logic [7:0] {
+    ACCEPTS_ROUND_LOTS = "Y",
+    NO_ORDER_SIZE_RESTRICTION = "N"
+  } dir_round_lot_size_e;
+
+  typedef enum logic [7:0] {
+    LIVE_PRODUCTION = "P",
+    TEST            = "T"
+  } dir_authenticity_e;
+
+  typedef enum logic [7:0] {
+    SST_RESTRICTED      = "Y",
+    SST_NOT_RESTRICTED  = "N"
+  } dir_short_sale_threshold_indicator_e;
+
+  typedef enum logic [7:0] {
+    IPO_SECURITY      = "Y",
+    NO_IPO_SECURITY   = "N"
+  } dir_ipo_flag_e;
+
+  typedef enum logic [7:0] {
+    TIER_1      = "1",
+    TIER_2      = "2"
+  } dir_LULUDReference_Price_Tier_e;
+
+  typedef enum logic [7:0] {
+    INSTRUMENT_ETP      = "Y",
+    INSTRUMENT_NOT_ETP  = "N"
+  } dir_ETP_flag_e;
+
+  typedef enum logic [7:0] {
+    ETP_INVERSE_ETP      = "Y",
+    ETP_NOT_INVERSE_ETP  = "N"
+  } dir_ETP_inverse_indicator_e;
+
+
+//Functions
+  function dir_market_category_e parse_market_category(logic[7:0] byte)
+    case(byte)
+      "Q" : return NASDAQ_GLOBAL_SELECT_MARKET;
+      "G" : return NASDAQ_GLOBAL_MARKET;
+      "S" : return NASDAQ_CAPTIAL_MARKET;
+      "N" : return NYSE;
+      "A" : return NYSE_AMERICAN;
+      "P" : return NYSE_ARCA;
+      "Z" : return BATS_Z_EXCHANGE;
+      "V" : return INVERSTORS_EXCHANGE;
+      default: return UNKNOWN_MARKET;
+    endcase
+  endfunction
+
+  function dir_financial_status_indicator_e parse_financial_status(logic[7:0] byte)
+    case(byte)
+      "D" : return DEFICIENT;
+      "E" : return DELINQUENT;
+      "Q" : return BANKRUPT;
+      "S" : return SUSPENDED;
+      "G" : return DEFICIENT_BANKRUPT;
+      "H" : return DEFICIENT_DELINQUENT;
+      "H" : return DELINQUENT_BANKRUPT;
+      "J" : return DELINQUENT_BANKRUPT;
+      "K" : return DEFICIENT_DELINQUENT_BANKRUPT;
+      "C" : return SUSPENDED_FOR_ETP;
+      "N" : return NORMAL;
+      default : return " ";
+    endcase
+  endfunction
+
+  function dir_financial_status_indicator_e parse_round_lots_only(logic[7:0] byte)
+    case(byte)
+      "Y" : return ACCEPTS_ROUND_LOTS;
+      "N" : return NO_ORDER_SIZE_RESTRICTION;
+    endcase
+  endfunction
+
+  function dir_authenticity_e parse_authenticity(logic[7:0] byte)
+    case(byte)
+      "P" : return LIVE_PRODUCTION;
+      "T" : return TEST;
+    endcase
+  endfunction
+
+    function dir_short_sale_threshold_indicator_e parse_SST_Indicator(logic[7:0] byte)
+    case(byte)
+      "P" : return SST_RESTRICTED;
+      "T" : return SST_NOT_RESTRICTED;
+    endcase
+  endfunction
+
+  function dir_ipo_flag_e parse_IPO_Flag(logic[7:0] byte)
+    case(byte)
+      "Y" : return IPO_SECURITY;
+      "N" : return NO_IPO_SECURITY;
+    endcase
+  endfunction
+
+  function dir_LULUDReference_Price_Tier_e parse_Price_Tier(logic[7:0] byte)
+    case(byte)
+      "1" : return TIER_1;
+      "2" : return TIER_2;
+    endcase
+  endfunction
+
+  function dir_ETP_flag_e parse_ETP_Flag(logic[7:0] byte)
+    case(byte)
+      "Y" : return INSTRUMENT_ETP;
+      "N" : return INSTRUMENT_NOT_ETP;
+    endcase
+  endfunction
+
+    function dir_ETP_inverse_indicator_e parse_ETP_inverse_indicator(logic[7:0] byte)
+    case(byte)
+      "Y" : return ETP_INVERSE_ETP;
+      "N" : return ETP_NOT_INVERSE_ETP;
+    endcase
+  endfunction
+endpackage
--- rtl/top.sv
+++ rtl/top.sv
@@ -1,5 +1,7 @@
 //NOTE ON CURRENT DESIGN : ASSUMPTION  CLK SPEED == INCOMING BYTE SEEED (will update later for more realistic results/methodologies)
+//Syntax note: variables ending with _e are enums
 
+import stock_dir_pckg::*;
 module top(
   input clk,
   input reset,
@@ -31,7 +33,7 @@ module top(
 
   typedef enum logic [3:0] {
     SYSTEM = "S",
-    STOCK_DIR,
+    STOCK_DIR = "R",
     ADD_ORDER_NO_MPID,
     ADD_ORDER_MPID,
     ORDER_EXEC,
@@ -41,15 +43,6 @@ module top(
     TRADE
   } message_states;
 
-  typedef enum logic [7:0] {
-    EVENT_START_MESSAGES = "O" ,
-    EVENT_START_SYSTEM   = "S" ,
-    EVENT_START_MARKET   = "Q" ,
-    EVENT_END_MARKET     = "M" ,
-    EVENT_END_SYSTEM     = "E" ,
-    EVENT_END_MESSAGES   = "C" ,
-
-  } system_event_codes;
 
 
   typedef enum logic [2:0] {
@@ -58,16 +51,55 @@ module top(
 
   } message_lengths;
 
-
+  //SYSTEM MESSAGE
 
   typedef struct packed {
-    logic [3:0] sys_message_type;
-    logic [7:0] sys_stock_locate;
-    logic [7:0] sys_tracking_number;
+    logic [7:0]  sys_message_type;
+    logic [15:0] sys_stock_locate;
+    logic [15:0] sys_tracking_number;
     logic [47:0] sys_time_stamp;
-    logic [3:0] sys_event_code;
+    logic [7:0]  sys_event_code;
   } System_Event_Message;
   System_Event_Message system_event_message;
+
+  typedef enum logic [7:0] {
+    EVENT_START_MESSAGES = "O" ,
+    EVENT_START_SYSTEM   = "S" ,
+    EVENT_START_MARKET   = "Q" ,
+    EVENT_END_MARKET     = "M" ,
+    EVENT_END_SYSTEM     = "E" ,
+    EVENT_END_MESSAGES   = "C" ,
+
+  } system_event_codes_e;
+
+
+
+
+
+    //STOCK DIRECTORY MESSAGE
+
+  typedef struct packed {
+    logic [7:0]  dir_message_type;
+    logic [15:0] dir_stock_locate;
+    logic [15:0] dir_tracking_number;
+    logic [47:0] dir_time_stamp;
+    logic [63:0] dir_stock_symbol;
+    logic [7:0]  dir_market_category;
+    logic [7:0]  dir_financial_status_indicator;
+    logic [31:0] dir_round_lot_size;
+    logic [7:0]  dir_round_lots_only;
+    logic [7:0]  dir_issue_classification;
+    logic [7:0]  dir_authenticity;
+    logic [7:0]  dir_short_sale_threshold_indicator;
+    logic [7:0]  dir_ipo_flag;
+    logic [7:0]  dir_LULUDReference_Price_Tier;
+    logic [7:0]  dir_ETP_flag;
+    logic [7:0]  dir_ETP_leverage_factor;
+    logic [7:0]  dir_ETP_inverse_indicator;
+  } Stock_Directory_Message;
+  Stock_Directory_Message stock_directory_message;
+
+
 
   always_ff @(clk) begin
     if(reset) begin
@@ -88,31 +120,13 @@ module top(
 
         //Generate payload length and packet end
         case (global_byte_counter)
-          42 : payload_len[15:8] <= pcap_byte; //Get length of payload
-          43 : payload_len[7:0]  <= pcap_byte;
-          44 : payload_len[15:8] <= payload_len[15:8] - 8; //Since UDP counts part of payload we minus 8
-          45 : packet_end <= global_byte_counter + packet_end;
-          46 : session[79:72] <= pcap_byte;
-          47 : session[71:64] <= pcap_byte;
-          48 : session[63:56] <= pcap_byte;
-          49 : session[55:48] <= pcap_byte;
-          50 : session[47:40] <= pcap_byte;
-          51 : session[39:32] <= pcap_byte;
-          52 : session[31:24] <= pcap_byte;
-          53 : session[23:16] <= pcap_byte;
-          54 : session[15:8]  <= pcap_byte;
-          55 : session[7:0]   <= pcap_byte;
-          56 : sequence_number[63:56] <= pcap_byte;
-          57 : sequence_number[55:48] <= pcap_byte;
-          58 : sequence_number[47:40] <= pcap_byte;
-          59 : sequence_number[39:32] <= pcap_byte;
-          60 : sequence_number[31:24] <= pcap_byte;
-          61 : sequence_number[31:24] <= pcap_byte;
-          62 : sequence_number[23:16] <= pcap_byte;
-          63 : sequence_number[15:8]  <= pcap_byte;
-          64 : sequence_number[7:0]   <= pcap_byte;
-          65 : message_count[15:8]    <= pcap_byte;
-          66 : message_count[7:0]     <= pcap_byte;
+          42,43: payload_len <= {payload_len[7:0], pcap_byte}; //Get length of payload
+          44: payload_len[15:8] <= payload_len[15:8] - 8; //Since UDP counts part of payload we minus 8
+          45: packet_end <= global_byte_counter + packet_end;
+          46,47,48,49,50,51,52,53,54,55: session <= {session[71:0], pcap_byte};
+          56,57,58,59,60,61,62,63,64: sequence_number <= {sequence_number[55:0], pcap_byte};
+          65,66: message_count <= {message_count[7:0], pcap_byte};
+
 
         endcase
 
@@ -124,24 +138,47 @@ module top(
             system_event_message.sys_message_type <= SYSTEM;
             message_type <= SYSTEM;
           end
+          if(pcap_byte == STOCK_DIR && !decoding_message) begin
+            internal_byte_counter <= 1;
+            decoding_message <= 1;
+            stock_directory_message.dir_message_type <= STOCK_DIR;
+            message_type <= STOCK_DIR;
+          end
 
           if(message_type == SYSTEM && decoding_message) begin
             internal_byte_counter <= internal_byte_counter + 1;
             case(internal_byte_counter)
-              1:  system_event_message.sys_stock_locate[15:8]    <= pcap_byte;
-              2:  system_event_message.sys_stock_locate[7:0]     <= pcap_byte;
-              3:  system_event_message.sys_tracking_number[15:8] <= pcap_byte;
-              4:  system_event_message.sys_tracking_number[7:0]  <= pcap_byte;
-              5:  system_event_message.sys_time_stamp[47:40]     <= pcap_byte;
-              6:  system_event_message.sys_time_stamp[39:32]     <= pcap_byte;
-              7:  system_event_message.sys_time_stamp[31:24]     <= pcap_byte;
-              8:  system_event_message.sys_time_stamp[23:16]     <= pcap_byte;
-              9:  system_event_message.sys_time_stamp[15:8]      <= pcap_byte;
+              1,2:  system_event_message.sys_stock_locate        <= {system_event_message.sys_stock_locate[7:0], pcap_byte};
+              3,4:  system_event_message.sys_tracking_number     <= {system_event_message.sys_tracking_number[7:0], pcap_byte};
+              5,6,7,8,9:   system_event_message.sys_time_stamp   <= {system_event_message.sys_time_stamp[39:0], pcap_byte};
               10: system_event_message.sys_time_stamp[7:0]       <= pcap_byte;
               11: system_event_message.sys_event_code[7:0]       <= pcap_byte;
             endcase
             if(internal_byte_counter == 11) decoding_message <= 0;
           end
+
+          if(message_type == STOCK_DIR && decoding_message) begin
+            case(internal_byte_counter)
+              1,2: stock_directory_message.dir_stock_locate <= {stock_directory_message.dir_stock_locate[7:0], pcap_byte};
+              3,4: stock_directory_message.dir_tracking_number <= {stock_directory_message.dir_tracking_number[7:0], pcap_byte};
+              5,6,7,8,9,10: stock_directory_message.dir_time_stamp <= {stock_directory_message.dir_time_stamp[39:0], pcap_byte};
+              11,12,13,14,15,16,17,18: stock_directory_message.dir_stock_symbol <= {stock_directory_message.dir_stock_symbol[55:0], pcap_byte};
+              19: stock_directory_message.dir_market_category  <= pcap_byte;
+              20: stock_directory_message.dir_financial_status_indicator  <= parse_financial_status(pcap_byte);
+              21,22,23,24,25,25: stock_directory_message.dir_round_lot_size <= {stock_directory_message.dir_round_lot_size[23:0], pcap_byte};
+              26: stock_directory_message.dir_issue_classification <= pcap_byte;
+              27: stock_directory_message.dir_authenticity <= parse_authenticity(pcap_byte);
+              28: stock_directory_message.dir_short_sale_threshold_indicator <= parse_SST_Indicator(pcap_byte);
+              29: stock_directory_message.dir_ipo_flag <= parse_IPO_Flag(pcap_byte);
+              30: stock_directory_message.dir_LULUDReference_Price_Tier <= parse_Price_Tier(pcap_byte);
+              31: stock_directory_message.dir_ETP_flag <= parse_ETP_Flag(pcap_byte);
+              32: stock_directory_message.dir_ETP_leverage_factor <= pcap_byte;
+              33: stock_directory_message.dir_ETP_inverse_indicator <= parse_ETP_inverse_indicator(pcap_byte);
+            endcase
+            internal_byte_counter <= internal_byte_counter + 1;
+            if(internal_byte_counter == 33) decoding_message <= 0;
+          end
+
 
         end
       end
